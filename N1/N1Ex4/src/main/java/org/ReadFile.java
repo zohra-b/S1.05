fc3d8a0b273a4edea7fa0d60eecb1e449e39bc33
zohra-b <z.bellamine@gmail.com>
2025-03-27 14:02:23 +0100

    n1 ex4 finished

---
+++ N1/N1Ex4/src/main/java/org/ReadFile.java
@@ -0,0 +1,67 @@
+package org;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+
+public class ReadFile {
+
+        public static void main(String[] args) throws IOException {
+            if (args.length != 1) {
+                System.out.println("You must enter a path as argument");
+            }
+            String indent = "";
+            File myDirectory = new File(args[0]);
+            if (!myDirectory.isDirectory() || !myDirectory.exists()) {
+                System.out.println("The argument is not a valid directory");
+            }
+
+
+            developingTree(myDirectory, "");
+
+        }
+
+
+public static void developingTree(File myDirectory, String indent) {
+    if (myDirectory.isDirectory()) {
+        File[] files = myDirectory.listFiles();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        if (files != null) {
+            for (File file : files){
+                if (file.isDirectory()) {
+                    System.out.println(indent + file.getName() + " " + sdf.format(file.lastModified()) + " (D)\r\n");;
+                    developingTree(file, indent + "   ");
+                } else {
+                    System.out.println(indent + file.getName() + " " + sdf.format(file.lastModified()) + " (F)\r\n");
+
+                }
+            }
+        }
+    } else {
+        try (BufferedReader reader = new BufferedReader(new FileReader(myDirectory.toString()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
+
+
+        public static Path settingDirectoryDocumentPath(){
+            String projectDir = System.getProperty("user.dir");
+            Path path = Paths.get(projectDir, "..", "..", "directory.txt");
+            return path;
+
+        }
+    }
+
+
